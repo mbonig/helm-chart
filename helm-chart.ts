021fe53888f1aa61a6fe4a08daed9eb4b743dec5
Matthew Bonig <matthew.bonig@gmail.com>
2020-10-13 15:49:20 -0600

    uses local helm when available.

--- helm-chart.ts
+++ helm-chart.ts
@@ -1,5 +1,5 @@
-import { Construct } from "constructs";
-import { ApiObject } from "cdk8s";
+import {Construct} from "constructs";
+import {ApiObject} from "cdk8s";
 import * as fs from "fs";
 import * as path from "path";
 
@@ -7,16 +7,26 @@ const cp = require('child_process');
 const yaml = require('js-yaml');
 
 interface HelmChartProps {
-  /*
+  /**
    * The absolute path to the chart that is being imported.
+   *
+   * @example "./mysql"
    */
   chart: string;
-  /*
-  * An optional string or object which provides all of the override values for the charts.
-  *
-  * If a string is provided than it must be relative to the chart path.
-  * */
+
+  /**
+   * An optional string or object which provides all of the override values for the charts.
+   *
+   * If a string is provided than it must be relative to the chart path.
+   * */
   values?: string | { [key: string]: any };
+
+  /**
+   * A local 'helm' command to run. If not found, it will fall back to using a docker image.
+   *
+   * @default "helm"
+   */
+  helmCmd?: string;
 }
 
 function stringToParams(params: string) {
@@ -27,12 +37,19 @@ function convertYamlDocsToObjects(docs: string[]) {
   return docs.filter((x: string) => !!x).map((doc: string) => yaml.safeLoad(doc))
 }
 
+function getAbsolutePath(chart: string) {
+  return chart.startsWith("/") ? chart : path.join(__dirname, chart);
+}
+
 export class HelmChart extends Construct {
   constructor(scope: Construct, id: string, props: HelmChartProps) {
     super(scope, id);
 
     let optionalValues = "";
     let additionalVolumes = "";
+    let overrideValuesLocation: string = "";
+    const canRunLocally = this.checkLocal(props.helmCmd);
+
     if (props.values) {
       if (typeof props.values === "string") {
         optionalValues = `-f ${props.values}`;
@@ -42,20 +59,36 @@ export class HelmChart extends Construct {
         if (!fs.existsSync(tempDir)) {
           fs.mkdirSync(tempDir);
         }
-        fs.writeFileSync(path.join(tempDir, 'overrides.yaml'), yaml.safeDump(props.values));
+        overrideValuesLocation = path.join(tempDir, 'overrides.yaml');
+        fs.writeFileSync(overrideValuesLocation, yaml.safeDump(props.values));
         additionalVolumes = `-v ${tempDir}:/props`;
-        optionalValues = `-f /props/overrides.yaml`;
+        optionalValues = canRunLocally ? `-f ${overrideValuesLocation}` : `-f /props/overrides.yaml`;
       }
     }
 
-    const dockerProcess = cp.spawnSync('docker', stringToParams(`run -v ${props.chart}:/app ${additionalVolumes} -w /app alpine/helm template ${optionalValues} .`));
+    let docs: string[];
 
-    let err = dockerProcess.stderr.toString();
-    if (err) {
-      throw new Error('An error occurred rendering the template: ' + err);
+    if (canRunLocally) {
+
+      const helmProcess = cp.spawnSync(props.helmCmd ?? "helm", stringToParams(`template ${optionalValues} .`), {cwd: props.chart});
+      const err = helmProcess.stderr.toString();
+      if (err) {
+        throw new Error('An error occurred rendering the template: ' + err);
+      }
+      docs = helmProcess.stdout.toString().split("---");
+
+    } else {
+
+      let absolutePath = getAbsolutePath(props.chart);
+      const dockerProcess = cp.spawnSync('docker', stringToParams(`run -v ${absolutePath}:/app ${additionalVolumes} -w /app alpine/helm template ${optionalValues} .`));
+
+      let err = dockerProcess.stderr.toString();
+      if (err) {
+        throw new Error('An error occurred rendering the template: ' + err);
+      }
+      docs = dockerProcess.stdout.toString().split("---");
     }
 
-    const docs = dockerProcess.stdout.toString().split("---");
     const convertedObjects = convertYamlDocsToObjects(docs);
 
     let importCount = 0;
@@ -63,5 +96,14 @@ export class HelmChart extends Construct {
       new ApiObject(this, `imported-${importCount}`, raw);
       importCount++;
     }
+  }
+
+  private checkLocal(helmCmd: string | undefined) {
+    const helmProcess = cp.spawnSync(helmCmd ?? "helm", ["version"]);
+    if (helmProcess.error) {
+      console.warn("Could not find local Helm. Falling back to docker. If you'd rather not use Docker, ensure 'helm' has been install and is available in the path. You can also override the helm command to use by specifying the `helmCmd` prop.");
+      return false;
+    }
+    return true;
   }
 }
--- main.test.ts
+++ main.test.ts
@@ -1,7 +1,7 @@
-import { Chart, Testing } from 'cdk8s';
-import { HelmChart } from "./helm-chart";
+import {Chart, Testing} from 'cdk8s';
+import {HelmChart} from "./helm-chart";
 import * as path from "path";
-
+import * as cp from 'child_process';
 
 describe('Values overrides', () => {
     test('no overrides', () => {
@@ -50,7 +50,44 @@ describe('Values overrides', () => {
       const deployment = results.find(x => x.kind === 'Deployment');
       expect(deployment.spec.template.spec.containers[0].image).toEqual("mysql:5.7.33");
     });
-
-
   }
 )
+
+
+function checkHelmLocal() {
+  const helmProcess = cp.spawnSync("helm");
+  if (helmProcess.error) {
+    throw new Error("You must have Helm installed locally to run these tests. https://helm.sh/docs/intro/install/")
+  }
+}
+
+
+describe('uses local vs docker', () => {
+
+
+  it('uses local when available', () => {
+    checkHelmLocal();
+
+    const app = Testing.app();
+    const chart = new Chart(app, 'test-chart');
+    new HelmChart(chart, 'test-helm-chart', {
+      chart: './mysql',
+    });
+
+
+    Testing.synth(chart); // no error here means success
+  });
+
+  it('uses docker when local not available', () => {
+
+    const app = Testing.app();
+    const chart = new Chart(app, 'test-chart');
+    new HelmChart(chart, 'test-helm-chart', {
+      chart: './mysql',
+      helmCmd: 'not-helm'
+    });
+
+    Testing.synth(chart); // no error here means success
+  });
+
+});
